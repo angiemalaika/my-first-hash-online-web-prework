fc9115f661b140dbc5980c92c1eb9dd17b0b8b5a
Angela Mumba <malaika.mumba@gmail.com>
2019-05-10 02:55:28 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,4 @@
-# use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
