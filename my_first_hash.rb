eed0be34d1f56a2379ddf8970565bc37ddc522cc
Angela Mumba <malaika.mumba@gmail.com>
2019-05-10 03:01:30 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,31 +20,18 @@ end
 
 def retrieval
   shipping_manifest =
-  {
-        "whale bone corsets" => 5, 
-        "porcelain vases" => 2, 
-        "oil paintings" => 3
-    }
- shipping_manifest["oil paintings"]
-  
-
+  {"whale bone corsets" => 5, 
+    "porcelain vases" => 2, 
+    "oil paintings" => 3}
+  shipping_manifest["oil paintings"]
 end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
- 
-
-
-hash["gun powder"] = 4
-  
-
-hash["muskets"] = 2
-
-  # return the shipping_manifest hash below
-"
-shipping_manifest
+ shipping_manifest =
+  {"whale bone corsets" => 5, 
+    "porcelain vases" => 2, 
+    "oil paintings" => 3}
+  hash["gun powder"] = 4
+  hash["muskets"] = 2
+  shipping_manifest
 end
